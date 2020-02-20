fafc13c68bdfeab845b2faa2314139f33f8ee725
osamamousa204 <osamamousa204@gmail.com>
2020-02-20 11:55:16 +0200

    sum(1)

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-    var theSum = a+b;
+function sum(x, y) { //eslint-disable-line
+    var theSum = x+y;
     // 'The sum of 4 and 7 is 11.'
-    var message = `The sum of ${a} and ${b} is ${theSum}.`;
+    var message = `The sum of ${x} and ${y} is ${theSum}.`;
     return [theSum, message];
 }
 testSum(4,7);
